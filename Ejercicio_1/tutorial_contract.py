f57e1d65fc15d967456d4ce2cebfbb499f107778
SmartyTM <luis.gomez@gft.com>
2024-10-14 11:53:39 -0500

    Más actualizaciones y más

---
+++ Ejercicio_1/tutorial_contract.py
@@ -0,0 +1,3 @@
+
+api = "4.0.0"  # Esto es un SmartContract usando el Contract Language  API 4.0.0
+version = "0.0.1"  # Usamos versionado semático. Revisar las buenas prácticas para su uso en https://semver.org/
